2209d6eb3b5f06a6a3765eaab8be464195e23700
evuez <helloevuez@gmail.com>
2021-11-26 11:20:09 -0500

    Parse Jest JSON output with jq

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,11 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('adds 1 + 2 to equal 4', () => {
+  expect(sum(1, 2)).toBe(4);
+});
+
+test('adds 1 + 2 to equal 5', () => {
+  expect(sum(1, 2)).toBe(5);
+});
