e7bb8f2603ffabce92d4c91776ab568dbc6191f9
evuez <helloevuez@gmail.com>
2021-11-28 16:11:03 -0500

    Clean up pipeline, fail on tests failure

--- sum.test.js
+++ sum.test.js
@@ -7,7 +7,3 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds 1 + 2 to equal 4', () => {
   expect(sum(1, 2)).toBe(4);
 });
-
-test('adds 1 + 2 to equal 5', () => {
-  expect(sum(1, 2)).toBe(5);
-});
